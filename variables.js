5c188dd5a9d9dd34d9d6548d19d21e520f14da36
Rakhi Jha <jdrakhi73@gmail.com>
2024-10-22 18:15:29 +0530

    Learn JavaScript

---
+++ variables.js
@@ -0,0 +1,18 @@
+var a = 20;
+var b = 30;
+var c=  a+b;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//let variable
+
+let name = "rakhi";
+let age = 20;
+console.log(name);
+console.log(age);
+
+//const
+const Name = "Rakhi Jha";
+console.log(Name);
