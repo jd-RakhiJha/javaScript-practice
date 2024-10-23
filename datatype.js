51e10195d7eadb50940ef13d99f943a77bf0f376
Rakhi Jha <jdrakhi73@gmail.com>
2024-10-23 18:27:07 +0530

    add new things

---
+++ datatype.js
@@ -0,0 +1,11 @@
+//tere are 8 datatype in js
+//Number, string, boolean, null, object, bigint, undefine, symbol 
+
+let number = 700;
+let  length = 7.80;
+
+let string = 'rakhi jha';
+let lastname = 'jhaji';
+
+let x = true;
+let y = false;
